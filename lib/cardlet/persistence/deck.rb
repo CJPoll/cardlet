7655cd17c50aeeb54d7ab3eb3feb3ad6e09a1582
Cody Poll <cjpoll@gmail.com>
2016-01-31 15:04:17 -0700

    Delete an arbitrary deck

--- lib/cardlet/persistence/deck.rb
+++ lib/cardlet/persistence/deck.rb
@@ -1,4 +1,3 @@
-require 'byebug'
 require 'fileutils'
 
 module Cardlet
@@ -8,6 +7,11 @@ module Cardlet
         FileUtils.mkdir_p(directory)
         file = File.open("#{directory}/#{deck.name}", 'w')
         file.write(deck.to_json)
+      end
+
+      def self.delete(name, directory)
+        filename = "#{directory}/#{name}"
+        File.delete(filename)
       end
     end
   end
