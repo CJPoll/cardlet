eaece4c1e02e43ec0a1a450605c83bfb074d8767
Cody Poll <cjpoll@gmail.com>
2016-01-31 17:56:24 -0700

    View questions, view decks, create & delete deck

--- lib/cardlet/persistence/deck.rb
+++ lib/cardlet/persistence/deck.rb
@@ -3,15 +3,25 @@ require 'fileutils'
 module Cardlet
   module Persistence
     module Deck
+      def self.delete(name, directory)
+        filename = "#{directory}/#{name}"
+        File.delete(filename)
+      end
+
+      def self.index(directory)
+        Dir.entries(directory).reject do |file|
+          file == '.' || file == '..'
+        end
+      end
+
+      def self.load(name, directory)
+        File.read("#{directory}/#{name}")
+      end
+
       def self.write(deck, directory)
         FileUtils.mkdir_p(directory)
         file = File.open("#{directory}/#{deck.name}", 'w')
         file.write(deck.to_json)
-      end
-
-      def self.delete(name, directory)
-        filename = "#{directory}/#{name}"
-        File.delete(filename)
       end
     end
   end
